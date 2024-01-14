25ef21a80236fa649baacf794fd9b79f1f6f6d39
Mustajab Ali <mustajabali.321@gmail.com>
2024-01-14 20:48:20 +0500

    Addedd About Me and Skills sections.
    Made some small changes too; added favicon for example

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.querySelector('.nav-links ul');
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+function showCard(cardType) {
+    if (cardType === 'languages') {
+        document.querySelector('.languages-card').style.display = 'flex';
+        document.querySelector('.tools-card').style.display = 'none';
+    } else if (cardType === 'tools') {
+        document.querySelector('.languages-card').style.display = 'none';
+        document.querySelector('.tools-card').style.display = 'flex';
+    }
+}
