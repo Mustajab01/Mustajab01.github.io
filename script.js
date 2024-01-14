2521eb478c8db72e170de7aff4c44231f1948934
Mustajab Ali <mustajabali.321@gmail.com>
2024-01-15 01:15:46 +0500

    Made some small UI Changes

--- script.js
+++ script.js
@@ -9,8 +9,12 @@ function showCard(cardType) {
     if (cardType === 'languages') {
         document.querySelector('.languages-card').style.display = 'flex';
         document.querySelector('.tools-card').style.display = 'none';
+        document.querySelector('#languages-btn').style.borderBottom = '2px solid black';
+        document.querySelector('#tools-btn').style.borderBottom = '2px solid #d2d2d2';
     } else if (cardType === 'tools') {
         document.querySelector('.languages-card').style.display = 'none';
         document.querySelector('.tools-card').style.display = 'flex';
+        document.querySelector('#languages-btn').style.borderBottom = '2px solid #d2d2d2';
+        document.querySelector('#tools-btn').style.borderBottom = '2px solid black';
     }
 }
