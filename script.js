760ca0fe26c5617bd6fa4b88d9eff31ed1f5ae20
Mustajab Ali <mustajabali.321@gmail.com>
2024-02-20 22:21:52 +0500

    Converted to Dark Mode

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ function showCard(cardType) {
     if (cardType === 'languages') {
         document.querySelector('.languages-card').style.display = 'flex';
         document.querySelector('.tools-card').style.display = 'none';
-        document.querySelector('#languages-btn').style.borderBottom = '2px solid black';
-        document.querySelector('#tools-btn').style.borderBottom = '2px solid #d2d2d2';
+        document.querySelector('#languages-btn').style.borderBottom = '2px solid #2014CC';
+        document.querySelector('#tools-btn').style.borderBottom = '2px solid #17171B';
+        document.querySelector('#tools-btn').style.borderBottom.hover = '2px solid #0A2472';
     } else if (cardType === 'tools') {
         document.querySelector('.languages-card').style.display = 'none';
         document.querySelector('.tools-card').style.display = 'flex';
-        document.querySelector('#languages-btn').style.borderBottom = '2px solid #d2d2d2';
-        document.querySelector('#tools-btn').style.borderBottom = '2px solid black';
+        document.querySelector('#languages-btn').style.borderBottom = '2px solid #17171B';
+        document.querySelector('#tools-btn').style.borderBottom = '2px solid #2014CC';
     }
 }
